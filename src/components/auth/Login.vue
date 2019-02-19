5d1fe7fb6b993504badf5fb0ed8129cf563267c6
Paulo Sergio Klaus <pauloklaus@tutanota.com>
2019-02-19 16:43:00 -0300

    Ajustes

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -8,20 +8,20 @@
                     </v-toolbar>
                     <v-card-text>
                         <v-form ref="formAuth">
-                            <v-text-field v-if="!haveRegister" v-model.trim="user.name" v-bind:rules="nameRules" prepend-icon="person" name="name" label="Nome Completo" type="Nome"/>
+                            <v-text-field v-if="!hasRegister" v-model.trim="user.name" v-bind:rules="nameRules" prepend-icon="person" name="name" label="Nome Completo" type="Nome"/>
                             <v-text-field v-model.trim="user.email" v-bind:rules="emailRules" prepend-icon="alternate_email" name="email" label="E-mail" type="text"/>
                             <v-text-field v-model.trim="user.password" prepend-icon="lock" name="password" label="Senha" id="password" type="password"/>
-                    <v-text-field v-if="!haveRegister" v-bind:rules="passwordConfirm" prepend-icon="lock" name="password_confirm" label="Confirme a Senha" id="password_confirm" type="password"/>
+                    <v-text-field v-if="!hasRegister" v-bind:rules="passwordConfirm" prepend-icon="lock" name="password_confirm" label="Confirme a Senha" id="password_confirm" type="password"/>
                         </v-form>
                     </v-card-text>
                     <v-card-actions>
                         <v-spacer/>
-                        <v-btn v-if="haveRegister" v-on:click="login()" class="white--text" color="teal darken-1">Entrar</v-btn>
+                        <v-btn v-if="hasRegister" v-on:click="login()" class="white--text" color="teal darken-1">Entrar</v-btn>
                         <v-btn v-else v-on:click="register()" class="white--text" color="teal darken-1">Cadastrar</v-btn>
                     </v-card-actions>
                     <v-toolbar dark color="teal darken-1">
-                        <a style="color: inherit" v-show="haveRegister" v-on:click="changeRegister(false)">Não tem cadastro? <strong>Cadastre-se!</strong></a>
-                        <a style="color: inherit" v-show="!haveRegister" v-on:click="changeRegister(true)">Já tem cadastro? <strong>Entre!</strong></a>
+                        <a style="color: inherit" v-show="hasRegister" v-on:click="changeRegister(false)">Não tem cadastro? <strong>Cadastre-se!</strong></a>
+                        <a style="color: inherit" v-show="!hasRegister" v-on:click="changeRegister(true)">Já tem cadastro? <strong>Entre!</strong></a>
                     </v-toolbar>
                 </v-card>
                 <v-alert :value="notify.show" :type="notify.type">{{ notify.message }}</v-alert>
@@ -31,7 +31,7 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
     name: "Login",
@@ -47,7 +47,7 @@ export default {
                 type: "error",
                 message: ""
             },
-            haveRegister: true,
+            hasRegister: true,
         }
     },
     computed: {
@@ -59,7 +59,7 @@ export default {
         emailRules() {
             const rules = []
             rules.push(v => (v || "") != "" || "E-mail é obrigatorio!");
-            rules.push(v =>  /.+@.+\.{1,}.+/.test((v || "")) || "E-mail invalido!");
+            rules.push(v =>  /.+@.+\.{1,}.+/.test((v || "")) || "E-mail inválido!");
             return rules;
         },
         passwordConfirm() {
@@ -70,8 +70,8 @@ export default {
 
     },
     methods: {
-        changeRegister(haveRegister) {
-            this.haveRegister = haveRegister
+        changeRegister(hasRegister) {
+            this.hasRegister = hasRegister
             this.notify.show = false
             this.notify.message = ""
         },
@@ -81,17 +81,15 @@ export default {
 
             this.$http.get("login", {headers: {"Authorization": "Basic " + btoa(this.user.email + ":" + this.user.password)}})
                 .then(responde => {
-                    this.notify.message= ""
-                    this.notify.show=false
+                    this.notify.message = ""
+                    this.notify.show = false
                     this.resetData()
                 })
                 .catch(error => {
-                    this.notify.type="error"
-                    this.notify.message= error.response.data.message
-                    this.notify.show=true
+                    this.notify.type = "error"
+                    this.notify.message = error.response.data.message
+                    this.notify.show = true
                 });
-
-
         },
         register() {
             if (!this.$refs.formAuth.validate())
@@ -99,10 +97,10 @@ export default {
 
             this.$http.post("login/create", {...this.user})
                 .then(responde => {
-                    this.notify.type="success"
-                    this.notify.message= "Cadastro efetuado com sucesso!"
-                    this.notify.show=true
-                    this.haveRegister = true;
+                    this.notify.type = "success"
+                    this.notify.message = "Cadastro efetuado com sucesso!"
+                    this.notify.show = true
+                    this.hasRegister = true;
                 })
                 .catch(error => {
                     this.notify.type="error"
